ac268bda78762cc075193f9a43d53328c5d25faa
DuyDucIU <bdduc682@gmail.com>
2026-01-27 23:22:12 +0700

    Initial commit

---
+++ thesis-management-system/src/main/java/iu/duyduc/thesis_management_system/ThesisManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package iu.duyduc.thesis_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThesisManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThesisManagementSystemApplication.class, args);
+	}
+
+}
---
+++ thesis-management-system/src/test/java/iu/duyduc/thesis_management_system/ThesisManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package iu.duyduc.thesis_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThesisManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
